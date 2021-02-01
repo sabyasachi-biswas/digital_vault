440190d9eb90fbc7b1157bf7f84e23b6f18e6138
Sabyasachi Biswas <49406254+sabyasachi-biswas@users.noreply.github.com>
2021-02-01 11:52:22 +0530

    change to gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,15 @@
 from tkinter import *
+from tkinter.font import Font
 
+# ------------------Header------------------------------------------
+font_head = Font(family = "", weight = "bold", size = 20)
+
+# ------------------Geometry------------------------------------------
 root = Tk()
 root.geometry('700x500')
+
+label_text = Label(root, text = "Login Page", anchor = CENTER, font = font_head)
+label_text.pack()
 
 
 root.mainloop()
