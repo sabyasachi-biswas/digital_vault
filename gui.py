aaec88f3f3bfb19994f7d61af33aba66b5a5a0a4
Sabyasachi Biswas <49406254+sabyasachi-biswas@users.noreply.github.com>
2021-02-01 12:29:55 +0530

    Changed window name to Login_page

--- gui.py
+++ gui.py
@@ -2,18 +2,18 @@ from tkinter import *
 from tkinter.font import Font
 
 # ------------------Geometry------------------------------------------
-root = Tk()
-root.geometry('500x250')
-root.minsize(400,170)
+login_page = Tk()
+login_page.geometry('500x250')
+login_page.minsize(400,170)
 
 # ------------------Header--------------------------------------------
 font_head = Font(family = "", weight = "bold", size = 20)
 
 #-------------------Buttons,Labels,etc..------------------------------
-label_text = Label(root, text = "Login Page", anchor = CENTER, font = font_head)
+label_text = Label(login_page, text = "Login Page", anchor = CENTER, font = font_head)
 label_text.pack()
 #username_frame
-frame_username = Frame(root)
+frame_username = Frame(login_page)
 frame_username.pack()
 
 label_login = Label(frame_username, text = "Username")
@@ -21,7 +21,7 @@ label_login.pack(side = LEFT)
 entry_username = Entry(frame_username)
 entry_username.pack()
 #password_frame
-frame_password = Frame(root)
+frame_password = Frame(login_page)
 frame_password.pack()
 
 label_password = Label(frame_password, text = "Password")
@@ -29,7 +29,7 @@ label_password.pack(side = LEFT)
 entry_password = Entry(frame_password, show = "*")
 entry_password.pack()
 #button_frame
-frame_button = Frame(root)
+frame_button = Frame(login_page)
 frame_button.pack()
 
 btn_login = Button(frame_button, text = "LOGIN")
@@ -38,4 +38,4 @@ btn_register = Button(frame_button, text = "REGISTER")
 btn_register.pack(side = BOTTOM)
 
 
-root.mainloop()
+login_page.mainloop()
